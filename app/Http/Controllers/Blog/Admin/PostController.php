5c1b389489a50e6a2abfd6892522131bc6caec50
Alex <aleksandrklembo2274@gmail.com>
2024-02-01 10:28:13 +0300

    Page edit post

--- app/Http/Controllers/Blog/Admin/PostController.php
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Repositories\BlogCategoryRepository;
 use App\Repositories\BlogPostRepository;
 use Illuminate\Http\Request;
 
@@ -19,6 +19,11 @@ class PostController extends BaseController
     private $blogPostRepository;
 
     /**
+     * @var BlogCategoryRepository
+     */
+    private $blogCategoryRepository;
+
+    /**
      * PostController constructor
      */
     public function __construct()
@@ -26,6 +31,7 @@ class PostController extends BaseController
         parent::__construct();
 
         $this->blogPostRepository = app(BlogPostRepository::class);
+        $this->blogCategoryRepository = app(BlogCategoryRepository::class);
     }
 
     public function index()
@@ -64,7 +70,16 @@ class PostController extends BaseController
      */
     public function edit(string $id)
     {
-        //
+        $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) {
+            abort(404);
+        }
+
+        $categoryList
+            = $this->blogCategoryRepository->getForComboBox();
+
+        return view('blog.admin.posts.edit',
+            compact('item', 'categoryList'));
     }
 
     /**
@@ -72,7 +87,7 @@ class PostController extends BaseController
      */
     public function update(Request $request, string $id)
     {
-        //
+        dd(__METHOD__, $request->all(), $id);
     }
 
     /**
@@ -80,6 +95,7 @@ class PostController extends BaseController
      */
     public function destroy(string $id)
     {
-        //
+        dd(__METHOD__, $id, request()->all());
+
     }
 }
---
+++ resources/views/blog/admin/posts/includes/result_messages.blade.php
@@ -0,0 +1,21 @@
+@if($errors->any())
+    <div class="row justify-content-center">
+        <div class="col-md-11">
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ $errors->first() }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+@endif
+
+@if(session('success'))
+    <div class="row justify-content-center">
+        <div class="col-md-11">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session()->get('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+@endif
